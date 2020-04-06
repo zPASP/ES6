dcaba6e7839eb11f8b96ca1aa02b896679af0b9f
Pedro Alexandre Pinto <zpasp@live.com>
2020-04-06 18:15:38 -0300

    WebPack Dev Server

---
+++ WebPack/src/main.js
@@ -0,0 +1,11 @@
+import { soma as sominha, sub } from './funcoes';
+import somaFunction, {soma3} from './soma';
+import * as somas from './soma';
+
+
+console.log(sominha(2,5))
+console.log(sub(8,1))
+console.log(somaFunction(1,6))
+console.log(soma3(2,2,3))
+
+console.log(somas.soma3(1,2,4))
---
+++ WebPack/src/soma.js
@@ -0,0 +1,7 @@
+export default function soma (a,b) {
+    return a + b;
+}
+
+export function soma3 (a,b,c) {
+    return a + b + c;
+}
